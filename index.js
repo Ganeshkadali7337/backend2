71bfb62fcaf26d377977b21559fe3a9bf8a00757
Ganeshkadali7337 <ganeshkadali0@gmail.com>
2024-04-21 09:35:32 +0530

    m

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const Product = require("./model");
+const cors = require("cors");
+
+app.use(cors());
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+mongoose
+  .connect("mongodb+srv://ganesh:ganesh@cluster7337.7exrzd7.mongodb.net/")
+  .then(() => console.log("db connected..."));
+
+app.post("/addproduct", async (req, res) => {
+  const { name } = req.body;
+  try {
+    const newData = new Product({ name });
+    await newData.save();
+    return res.send("Product added");
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
+app.get("/", async (req, res) => {
+  try {
+    const data = await Product.find();
+    return res.send(data);
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+app.listen(PORT, () => console.log("server running..."));
